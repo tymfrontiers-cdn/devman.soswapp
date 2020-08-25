ac7a322889a3c5b012ce5ac29cb9c972021b942a
Ikechukwu Okalia <dev@ikechukwuokalia.com>
2020-08-26 00:49:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+require_once "app.init.php";
+require_once APP_BASE_INC;
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr" manifest="./site.webmanifest">
+  <head>
+    <meta charset="utf-8">
+    <title>7 OS Web App</title>
+    <?php include PRJ_INC_ICONSET; ?>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
+    <meta name="keywords" content="7, seven, os, operating, system, template">
+    <meta name="description" content="7 OS Web - app template">
+    <meta name="author" content="Your developer">
+    <meta name="creator" content="7 OS">
+    <meta name="publisher" content="Your company">
+    <meta name="robots" content='index'>
+    <!-- Theming styles -->
+    <link rel="stylesheet" href="<?php echo WHOST; ?>/7os/theme-soswapp/css/theme.min.css">
+    <link rel="stylesheet" href="<?php echo WHOST; ?>/7os/theme-soswapp/css/theme-<?php echo PRJ_THEME; ?>.min.css">
+    <!-- optional plugin -->
+    <link rel="stylesheet" href="<?php echo WHOST; ?>/7os/plugin-soswapp/css/plugin.min.css">
+    <link rel="stylesheet" href="<?php echo WHOST; ?>/7os/dnav-soswapp/css/dnav.min.css">
+    <link rel="stylesheet" href="<?php echo WHOST; ?>/7os/faderbox-soswapp/css/faderbox.min.css">
+    <!-- Project styling -->
+    <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
+  </head>
+  <body>
+    <?php include PRJ_INC_HEADER; ?>
+    <section id="main-content">
+      <h1>7 OS Web - App</h1>
+      <p>Here is a sample web app</p>
+    </section>
+    <?php include PRJ_INC_FOOTER; ?>
+    <!-- Required scripts -->
+    <script src="<?php echo WHOST; ?>/7os/jquery-soswapp/js/jquery.min.js">  </script>
+    <script src="<?php echo WHOST; ?>/7os/js-generic-soswapp/js/js-generic.min.js">  </script>
+    <script src="<?php echo WHOST; ?>/7os/theme-soswapp/js/theme.min.js"></script>
+    <!-- optional plugins -->
+    <script src="<?php echo WHOST; ?>/7os/plugin-soswapp/js/plugin.min.js"></script>
+    <script src="<?php echo WHOST; ?>/7os/dnav-soswapp/js/dnav.min.js"></script>
+    <script src="<?php echo WHOST; ?>/7os/faderbox-soswapp/js/faderbox.min.js"></script>
+    <!-- project scripts -->
+    <script src="<?php echo \html_script ("base.min.js"); ?>"></script>
+  </body>
+</html>
